4c447bfa33bf824b74ccdbf8acaa81b040097e84
Scott Ingram <scott4ai@users.noreply.github.com>
2024-05-14 03:46:09 -0400

    Add other Hugging Face model download methods

--- 3-downloadmodel.sh
+++ 3-downloadmodel.sh
@@ -12,7 +12,18 @@ brew install git-lfs
 git lfs install
 # Update Git hooks.
 sudo git lfs install --system
-# With SSH, clone the pre-trained model repo
-# git clone git@hf.co:scott4ai/llama3-8b-cosmic-fusion-dynamics-f16-gguf model
-# With HTTPS, clone the pre-trained model repo
-git clone https://huggingface.co/scott4ai/llama3-8b-cosmic-fusion-dynamics-f16-gguf model
+
+# Use any one of the following commands to download the model. All require git lfs to be installed.
+# If any of these protocols ask for a password, you can use your Hugging Face token in lieu of password.
+
+# Uses SSH and shows a progress report as it downloads
+git lfs clone git@hf.co:scott4ai/llama3-8b-cosmic-fusion-dynamics-f16-gguf model
+
+# Uses SSH and doesn't not show a progress report
+git clone git@hf.co:scott4ai/llama3-8b-cosmic-fusion-dynamics-f16-gguf model
+
+# Uses HTTPS and shows a progress report as it downloads
+git lfs clone https://hf.co/scott4ai/llama3-8b-cosmic-fusion-dynamics-f16-gguf model
+
+# Uses HTTPS and doesn't show a progress report
+git clone https://hf.co/scott4ai/llama3-8b-cosmic-fusion-dynamics-f16-gguf model
